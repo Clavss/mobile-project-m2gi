425827209e66c9467168863abb1be4783d63f846
Antoine C <antclavs@gmail.com>
2021-01-27 16:47:34 +0100

    rebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,27 @@ import {StatusBar} from '@ionic-native/status-bar/ngx';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {AngularFirestoreModule} from "@angular/fire/firestore";
+import {AngularFireModule} from "@angular/fire";
+import {environment} from "../environments/environment";
+import {AngularFireAnalyticsModule} from "@angular/fire/analytics";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-    ],
-    entryComponents: [],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FontAwesomeModule],
-    providers: [
-        StatusBar,
-        SplashScreen,
-        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
-    ],
-    bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+	],
+	entryComponents: [],
+	imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FontAwesomeModule,
+		AngularFireModule.initializeApp(environment.firebaseConfig),
+		AngularFireAnalyticsModule,
+		AngularFirestoreModule
+	],
+	providers: [
+		StatusBar,
+		SplashScreen,
+		{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule {
 }
