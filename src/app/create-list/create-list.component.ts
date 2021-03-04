27da62b637973f47801766362a14f0c9956fcfc2
Thomas <therve38@gmail.com>
2021-03-04 22:32:39 +0100

    Ajout d'un peu de css pour la taille des elements d'une liste

--- src/app/create-list/create-list.component.ts
+++ src/app/create-list/create-list.component.ts
@@ -1,40 +1,40 @@
 import {Component, OnInit} from '@angular/core';
-import {ListService} from "../services/list.service";
-import {ModalController} from "@ionic/angular";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {List} from "../models/list";
+import {ListService} from '../services/list.service';
+import {ModalController} from '@ionic/angular';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {List} from '../models/list';
 
 @Component({
-	selector: 'app-create-list',
-	templateUrl: './create-list.component.html',
-	styleUrls: ['./create-list.component.scss'],
+    selector: 'app-create-list',
+    templateUrl: './create-list.component.html',
+    styleUrls: ['./create-list.component.scss'],
 })
 export class CreateListComponent implements OnInit {
 
-	createListForm: FormGroup;
+    createListForm: FormGroup;
 
-	constructor(private listService: ListService,
-							private modalCtrl: ModalController,
-							private fb: FormBuilder) {
-	}
+    constructor(private listService: ListService,
+                private modalCtrl: ModalController,
+                private fb: FormBuilder) {
+    }
 
-	ngOnInit() {
-		this.createListForm = this.fb.group({
-			name: ['', [Validators.required, Validators.minLength(2)]],
-		})
-	}
+    ngOnInit() {
+        this.createListForm = this.fb.group({
+            name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(18)], ],
+        });
+    }
 
-	dismissModal() {
-		this.modalCtrl.dismiss();
-	}
+    dismissModal() {
+        this.modalCtrl.dismiss();
+    }
 
-	create(): void {
-		this.listService.createList(new List(this.createListForm.get('name').value));
-		this.dismissModal();
-	}
+    create(): void {
+        this.listService.createList(new List(this.createListForm.get('name').value));
+        this.dismissModal();
+    }
 
-	get errorControl() {
-		return this.createListForm.controls;
-	}
+    get errorControl() {
+        return this.createListForm.controls;
+    }
 
 }
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-	production: false
-};
