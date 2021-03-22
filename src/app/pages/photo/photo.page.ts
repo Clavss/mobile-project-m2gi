140f7a7056db9f23975d1ffac721125b6b51f8c4
ThomasHerve <therve38@gmail.com>
2021-03-22 17:37:31 +0100

    photos coté front

--- src/app/pages/photo/photo.page.ts
+++ src/app/pages/photo/photo.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {PhotoService} from '../../services/photo.service';
+import {ModalController} from '@ionic/angular';
 
 @Component({
     selector: 'app-photo',
@@ -8,16 +9,39 @@ import {PhotoService} from '../../services/photo.service';
 })
 export class PhotoPage implements OnInit {
 
-    constructor(public photoService: PhotoService) {
+    constructor(public photoService: PhotoService, public modalController: ModalController) {
     }
 
-    ngOnInit() {
+    photoDict: Map<number, boolean>;
 
+    ngOnInit() {
+        this.photoDict = new Map();
     }
 
     addPhotoToGallery() {
         this.photoService.addNewToGallery();
     }
 
+    supprPhoto(index: number) {
+        this.photoDict.set(index, false);
+        this.photoService.removeFromGallery(index);
+    }
 
+    showButton(position: number) {
+        this.photoDict.forEach(
+            (v, k, m) => {
+                if (k !== position) {
+                    m.set(k, false);
+                }
+            }
+        );
+        this.photoDict.set(position, !this.photoDict.get(position));
+    }
+
+    buttonState(position: number): boolean {
+        if (!this.photoDict.has(position)) {
+            this.photoDict.set(position, false);
+        }
+        return this.photoDict.get(position);
+    }
 }
