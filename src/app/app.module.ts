2d527bdcab766c917ae5f4458e46243996b9cc57
ThomasHerve <therve38@gmail.com>
2021-03-22 14:28:46 +0100

    corrections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,30 +8,31 @@ import {StatusBar} from '@ionic-native/status-bar/ngx';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
-import {AngularFirestoreModule} from "@angular/fire/firestore";
-import {AngularFireModule} from "@angular/fire";
-import {environment} from "../environments/environment";
-import {AngularFireAnalyticsModule} from "@angular/fire/analytics";
-import { ServiceWorkerModule } from '@angular/service-worker';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {AngularFireAnalyticsModule} from '@angular/fire/analytics';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-	],
-	entryComponents: [],
-	imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FontAwesomeModule,
-		AngularFireModule.initializeApp(environment.firebaseConfig),
-		AngularFireAnalyticsModule,
-		AngularFirestoreModule,
-		ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-	],
-	providers: [
-		StatusBar,
-		SplashScreen,
-		{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
-	],
-	bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    entryComponents: [],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FontAwesomeModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireAnalyticsModule,
+        AngularFirestoreModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}), FormsModule,
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
