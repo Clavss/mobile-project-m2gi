746476db6aebf01f2c2a3b4db2c2a43f836df671
ThomasHerve <therve38@gmail.com>
2021-03-22 15:49:10 +0100

    ecran photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import {Platform} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
 import firebase from 'firebase';
-import {Router} from '@angular/router';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs/operators';
 
 
 @Component({
@@ -16,15 +17,29 @@ export class AppComponent {
         private platform: Platform,
         private splashScreen: SplashScreen,
         private statusBar: StatusBar,
-        private route: Router
+        private route: Router,
     ) {
         this.initializeApp();
+        route.events.pipe(
+            filter(event => event instanceof NavigationEnd)
+        ).subscribe(event => {
+            event = event as NavigationEnd;
+            if (event.url.slice(1) === 'photo') {
+                this.todolist = false;
+            } else {
+                this.todolist = true;
+            }
+        });
+
     }
+
+    todolist: boolean;
 
     initializeApp() {
         this.platform.ready().then(() => {
             this.statusBar.styleDefault();
             this.splashScreen.hide();
+            this.todolist = true;
         });
     }
 
@@ -36,5 +51,17 @@ export class AppComponent {
         firebase.auth().signOut()
             .then(() => this.route.navigate(['/login']))
             .catch((error) => alert(error.message));
+    }
+
+    isTodo(): boolean {
+        return this.todolist;
+    }
+
+    goTodo(): void {
+        this.route.navigate(['/home']);
+    }
+
+    goPhoto(): void {
+        this.route.navigate(['/photo']);
     }
 }
---
+++ src/app/pages/photo/photo.page.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-photo',
+    templateUrl: './photo.page.html',
+    styleUrls: ['./photo.page.scss'],
+})
+export class PhotoPage implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+
+    }
+
+}
