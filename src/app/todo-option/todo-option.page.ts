ed04ae16a0bdb601dcde0e5e1fb1e0bfbe67a5fe
ThomasHerve <therve38@gmail.com>
2021-03-23 21:47:40 +0100

    almost done

--- src/app/todo-option/todo-option.page.ts
+++ src/app/todo-option/todo-option.page.ts
@@ -2,23 +2,45 @@ import {Component, Input, OnInit} from '@angular/core';
 import {ModalController} from '@ionic/angular';
 import {ListDetailsPage} from '../pages/list-details/list-details.page';
 import {List} from '../models/list';
+import {ListService} from '../services/list.service';
 
 @Component({
-  selector: 'app-todo-option',
-  templateUrl: './todo-option.page.html',
-  styleUrls: ['./todo-option.page.scss'],
+    selector: 'app-todo-option',
+    templateUrl: './todo-option.page.html',
+    styleUrls: ['./todo-option.page.scss'],
 })
 export class TodoOptionPage implements OnInit {
 
-  @Input() list: List;
-  @Input() parent: ListDetailsPage;
+    @Input() list: List;
+    @Input() parent: ListDetailsPage;
 
-  constructor(public modalCtrl: ModalController) { }
+    error: boolean;
 
-  ngOnInit() {
-  }
-  dismiss() {
-    this.modalCtrl.dismiss();
-  }
+    constructor(public modalCtrl: ModalController,
+                private listService: ListService) {
+
+    }
+
+    ngOnInit() {
+        this.error = false;
+    }
+
+    dismiss() {
+        this.modalCtrl.dismiss();
+    }
+
+    checkEmail(v: string) : boolean {
+
+        return true;
+    }
+
+    addWrite(v: string) {
+        this.listService.updateListNewWriter(v, this.list);
+    }
+
+    addRead(v: string) {
+        this.listService.updateListNewReader(v, this.list);
+    }
+
 
 }
